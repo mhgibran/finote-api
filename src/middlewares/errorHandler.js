1038396ff4415fa2aa97ded27ef6ea33c8377b5c
Muammar Haekal Gibran <48083336+mhgibran@users.noreply.github.com>
2024-05-26 14:52:30 +0700

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,8 @@
+const { build } = require("../config/env");
+
 module.exports = (err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message || "Internal Server Error",
-    error: process.env.NODE_ENV === "development" ? err : {},
+    error: build === "development" ? err : {},
   });
 };
